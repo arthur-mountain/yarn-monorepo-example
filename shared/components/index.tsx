5fcf9f461ccea1f858f930fa27512c70b1c6554f
arthur <arthur.oceanus@gmail.com>
2024-05-20 12:09:06 +0800

    FIXME: nextjs build error

--- shared/components/index.tsx
+++ shared/components/index.tsx
@@ -1,1 +1,5 @@
+// This will imported error
+// export * from "./Button";
+// THis will imported successfully but warning in console
+// export { default as Button} from "./Button";
 export { Button } from "./Button";
