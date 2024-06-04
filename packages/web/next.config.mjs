c8b1fe2dc7810e7da2c5652d5f322f27e1b73251
arthur <arthur.oceanus@gmail.com>
2024-06-04 10:36:27 +0800

    remove rnw related, just build normal react component currently

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["react-native$"] = "react-native-web";
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- vitest.config.base.ts
+++ vitest.config.base.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    include: ["__tests__/**/*.test.{ts,tsx,js,jsx}"],
     coverage: { provider: "v8" },
   },
 });
