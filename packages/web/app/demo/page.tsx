7566db053ee9a2c30d68272a68637744502927b6
arthur <arthur.oceanus@gmail.com>
2024-06-08 17:48:55 +0800

    update web build commands

--- packages/web/app/demo/page.tsx
+++ packages/web/app/demo/page.tsx
@@ -1,11 +1,10 @@
-"use client";
 import { Button } from "@yarn-monorepo-example/components";
 
 export default function Demo() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Demo
-      <Button label="demo" onPress={() => {}} />
+      <Button>demo</Button>
     </main>
   );
 }
